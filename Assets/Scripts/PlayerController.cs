d4fd27e9c63b4b382f6ac4741bce87c3a481a126
ARAV1NDPC\Aravindan <aravindanputhalath@gmail.com>
2024-12-13 11:45:56 +0530

    Mini Assignment 2 - Scripting Animation & Flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
+
+        if(animator != null )
+        {
+            Debug.Log("animator is attached correctly");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(speed);
+        }
+        transform.localScale = scale;
     }
 }
