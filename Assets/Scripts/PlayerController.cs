5904d39c5ba4af4bf5b67587655276a98be2ba23
ARAV1NDPC\Aravindan <aravindanputhalath@gmail.com>
2024-12-14 13:04:47 +0530

    Mini Assignment 1 - Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,62 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private Animator animator;
+    private Rigidbody2D Rb2D;
+    public float speed = 0;
+    public float jump = 0;
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
+        Rb2D = gameObject.GetComponent <Rigidbody2D>();
     }
 
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        PlayerMovementAnimation(horizontal, vertical);
+        MoveCharacter(horizontal, vertical);
+    }
 
+    private void MoveCharacter(float horizontal, float vertical)
+    {
+       Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        if (vertical > 0)
+        {
+            Rb2D.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+    }
+
+    private void PlayerMovementAnimation(float horizontal,float vertical)
+    {   
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
-            scale.x = Mathf.Abs(speed);
+            scale.x = Mathf.Abs(horizontal);
         }
         transform.localScale = scale;
+        animator.SetBool("Crouch", Input.GetKey(KeyCode.C));
 
-        animator.SetBool("Crouch", Input.GetKey(KeyCode.C)) ;
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
     }
 }
